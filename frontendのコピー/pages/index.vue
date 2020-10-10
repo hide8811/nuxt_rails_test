bcdaf2d12d738f3f512f94f342dca59bbb0de96f
KohnoTaiyo <taiyowork66@gmail.com>
2020-10-10 13:37:12 +0900

    受け取り側の修正

---
+++ frontendのコピー/pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <button
+      type="button"
+      name="button"
+      @click="getMsg"
+    >
+      RailsからAPIを取得する
+    </button>
+    <div
+      v-for="(msg, i) in msgs"
+      :key="i"
+    >
+      {{ msg }}
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data () {
+    return {
+      msgs: []
+    }
+  },
+  methods: {
+    getMsg () {
+      axios.get('/api/v1/hello')
+        .then(res => this.msgs.push(res))
+    }
+  }
+}
+</script>
